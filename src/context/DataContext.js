d6a00c3319f253ef150a28e6da8b9d1eb2e505bd
ETIKSUR <suryateja.etikala@hilti.com>
2023-01-22 16:09:20 +0530

    final before ui revamp

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -14,100 +14,22 @@ export const DataProvider = ({ children }) => {
     const [auth, setAuth] = useState(null);
     const [sess,setSess] =useState(false);
 
-    const handleSetAuth=(auth)=>{
-          setAuth(auth);
-    }
-    const handleSetBooks=(books)=>{
-          setBooks(books)
-    }
-    const handleExpandClick = (book) => {
-        if (expanded==null){
-          console.log(book);
-          setExpanded(book[0])
-        }
-        else {
-          setExpanded(null)
-        }
-        
-      };
-      const [editBook,setEditBook] = useState(null);
-
-  const fetchBooks = async () => {
-    try {
-      const newbooklist = await GetBooks()
-      setBooks(newbooklist);
-    } catch (err) {
-    }
-  }
-
-  const fetchGoogleBooks= async ()=>{
-    if (search!=''){
-    try {
-      const newbooklist=await GetGoogleBooks(search)
-      setBooks(newbooklist);
-    }
-    catch(err){
-
-    }
-  }
-  }
-
-  useEffect(() => {
-    fetchBooks();
-  }, [])
-
-  useEffect(()=>{
-    fetchGoogleBooks();
-  },[search])
 
 
-  const handleEditBook =(book)=>{
-    setExpanded(null)
-    if (book==null){
-      setEditBook(null)
-    }
-    else {
-      setEditBook(book[0])
-    }
-    
-  }
 
-const handleNewBook=(book)=>{
-  console.log(book)
-  const AddBook = async () => {
-    try {
-      const newbooklist = await PostBook(book)
-      fetchBooks();
-      //setBooks(newbooklist);
-    } catch (err) {
-    }
-  }
 
-  AddBook();
-  //const bookslist=[...books,book];
-  //setBooks(bookslist);
-}
-const handleAddBookState=()=>{
-        setExpanded(null);
-      setaddBookState(!addBookState);
-}
-let contentstyle={
-}
-if (addBookState || expanded || editBook){
-  contentstyle={
-    filter:'blur(10px)',
-    PointerEvent :'none'
-}
-}
+
+
+
+
+
+
 
 
     return (
         <DataContext.Provider value={{
             books, setBooks, search, setSearch, expanded, setExpanded, addBookState, setaddBookState,
              newBook, setNewBook, auth, setAuth, sess, setSess,
-            handleExpandClick, contentstyle,
-            handleNewBook, handleAddBookState, handleEditBook,
-            handleSetBooks, handleSetAuth
 
         }}>
             {children}
---
+++ src/store/SearchSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit"
+import GetGoogleBooks from "../apis/GetGoogleBooks";
+import { setBooks } from "./BooksSlice";
+
+const initialState={
+
+    search:'',
+    
+
+}
+const SearchSlice = createSlice({
+    name :'search',
+    initialState,
+    reducers:{
+
+        setSearch:(state,action)=>{
+            state.search=action.payload;
+
+        }
+
+
+    }
+
+})
+export const fetchGoogleBooks=(search)=> async dispatch =>{
+    try {
+        const response= await GetGoogleBooks(search);
+        //console.log(response)
+        dispatch(setBooks(response));
+    }
+    catch(e){}
+}
+export const getSearch = (state) => state.search.search;
+export const { setSearch } = SearchSlice.actions
+export default SearchSlice.reducer
